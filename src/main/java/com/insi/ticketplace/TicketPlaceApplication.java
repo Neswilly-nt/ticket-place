5a2d1e41a8b9f8aff3056c7a90ccdc29f6ea18a4
Neswilly <jeannotneswilly@gmail.com>
2026-03-16 21:14:32 +0300

    feat: initialize Spring Boot project

---
+++ src/main/java/com/insi/ticketplace/TicketPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.insi.ticketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketPlaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/insi/ticketplace/TicketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insi.ticketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
